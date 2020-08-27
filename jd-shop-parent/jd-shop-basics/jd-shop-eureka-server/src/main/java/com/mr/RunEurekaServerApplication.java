b1feacfc88663b915b9464cdc1bcceb427827049
wangshuangquan <s15639308223>
2020-08-27 19:36:25 +0800

    first commit

---
+++ jd-shop-parent/jd-shop-basics/jd-shop-eureka-server/src/main/java/com/mr/RunEurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.mr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @version 1.0
+ * @author:王双全
+ * @date: 2020-30-27 15:30
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class RunEurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunEurekaServerApplication.class);
+    }
+}
